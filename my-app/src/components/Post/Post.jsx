da8e4530152082752f8fc30c824a008b332095fb
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-12-27 02:47:05 +0600

    post design

---
+++ my-app/src/components/Post/Post.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Divider from '../Divider/Divider';
+
+const Post = () => {
+    return (
+      <div>
+        <Divider></Divider>
+        <h2 className="text-center text-xl mb-2 text-info">Create a post...</h2>
+        <form className="w-3/4 mx-auto">
+          <div className="text">
+            <textarea
+              className="textarea textarea-info w-full h-32 default-bg text-info text-xl"
+              placeholder="Write here...."
+            ></textarea>
+          </div>
+          <div className="photo text-center">
+            <input
+              type="file"
+              className="file-input file-input-bordered file-input-info default-bg text-info"
+              accept=".jpg"
+            />
+          </div>
+          <div className="submit text-center my-4">
+            <button className="btn btn-info text-info  default-bg">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+};
+
+export default Post;
--- my-app/src/components/home/Home.jsx
+++ my-app/src/components/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../Footer/Footer';
 import Nav from '../nav/nav';
+import Post from '../Post/Post';
 const Home = () => {
     return (
       <div>
         <Nav></Nav>
+        <Post></Post>
         <Footer></Footer>
       </div>
     );
