e7e6537ce4f4da8b77a36bf20f3a181d2fa4c674
AkashChakrabortty <akashchakrabortty@gmail.com>
2022-12-28 01:18:21 +0600

    comment

--- my-app/src/components/DetailsCard/DetailsCard.jsx
+++ my-app/src/components/DetailsCard/DetailsCard.jsx
@@ -3,12 +3,11 @@ import { AiTwotoneHeart } from "react-icons/ai";
 import { useLoaderData } from 'react-router-dom';
 import { userInfo } from '../../context/AuthProvider';
 import Divider from '../Divider/Divider';
-
 const DetailsCard = () => {
     const loaderData =  useLoaderData();
     const {user} = useContext(userInfo)
-  
-    const handleReaction = (post) => {
+    // const notify = (a) => toast(a);
+    const handleReaction = () => {
       const loveInfo = {
         post_owner: loaderData.user_name,
         post_owner_email: loaderData.user_email,
@@ -30,16 +29,53 @@ const DetailsCard = () => {
       })
         .then((res) => res.json())
         .then((data) => {
-        //   if (data.acknowledged === false) {
-        //     // setReFetch(!reFetch);
-        //   } else {
-        //     // setReFetch(!reFetch);
-        //   }
-        console.log(data)
+          if (data.acknowledged === false) {
+            // notify('Love remove')
+            // setReFetch(!reFetch);
+          } else {
+            //  notify("Love");
+            // setReFetch(!reFetch);
+          }
         });
 
 
     };
+
+     const handleForm = (event) => {
+       event.preventDefault();
+       const text = event.target.text.value;
+       if (text.trim().length === 0) {
+        alert(`You can't comment empty`);
+       } else {
+         const milliseconds = new Date().getTime();
+
+         const commentInfo = {
+           post: loaderData.post,
+           post_photo: loaderData.post_photo,
+           post_owner_email: loaderData.user_email,
+           post_owner_name: loaderData.user_name,
+           post_owner_photo: loaderData.user_photo,
+           milliseconds: milliseconds,
+           previous_id: loaderData._id,
+           comment_giver_name: user.displayName,
+           comment_giver_email: user.email,
+           comment_giver_photo: user.photoURL,
+           comment: text,
+         };
+         //insert db
+         fetch("http://localhost:5000/comment", {
+           method: "POST",
+           headers: {
+             "content-type": "application/json",
+           },
+           body: JSON.stringify(commentInfo),
+         })
+           .then((res) => res.json())
+           .then((data) => {
+             event.target.reset();
+           });
+       }
+     };
     return (
       <div className="w-9/12 mx-auto border-2 border-info p-4 rounded text-info default-bg">
         <div className="avatar">
@@ -59,18 +95,28 @@ const DetailsCard = () => {
           />
         </div>
         <div className="reaction my-3">
-          <button className="btn btn-outline btn-info text-xl mb-2" onClick={handleReaction}>
+          <button
+            className="btn btn-outline btn-info text-xl mb-2"
+            onClick={handleReaction}
+          >
             <AiTwotoneHeart></AiTwotoneHeart>
           </button>
-          <textarea
-            className="textarea textarea-info w-full h-20 default-bg text-info"
-            name="text"
-            placeholder="Write here...."
-          ></textarea>
+          <form onSubmit={handleForm} className='flex gap-2 items-center'>
+            <textarea
+              className="textarea textarea-info w-full h-20 default-bg text-info"
+              name="text"
+              placeholder="Write here...."
+            ></textarea>
+            <button
+              className="btn btn-outline btn-info mb-2"
+              type='submit'
+            >
+             Comment
+            </button>
+          </form>
         </div>
-        <div className="all-comments">
-            
-        </div>
+        <Divider></Divider>
+        <div className="all-comments"></div>
       </div>
     );
 };
